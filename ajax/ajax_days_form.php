083ea9ad5689c309e500caeed5b0fba1864120af
HP6300PRO <strecenko.sergej@gmail.com>
2020-09-23 08:38:12 +0300

    Восстановил выбор дня

--- ajax/ajax_days_form.php
+++ ajax/ajax_days_form.php
@@ -10,7 +10,7 @@ while($ob = $res->GetNextElement()){
 }
 $week = array(1=> 'Понедельник' , 'Вторник' , 'Среда' , 'Четверг' , 'Пятница' , 'Суббота' , 'Воскресенье' );
 $begin = new DateTime( date('Y-m-d') );
-$end = new DateTime( date('Y-m-d', strtotime('+'.$arProps["PERIOD"]["VALUE"].' days')));
+$end = new DateTime( date('Y-m-d', strtotime('+14 days')));
 $end = $end->modify( '+1 day' );
 
 $interval = new DateInterval('P1D');
