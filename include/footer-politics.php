4593d7af87cd6c60c03f65922c62b246affc3cc4
HP6300PRO <strecenko.sergej@gmail.com>
2020-11-30 09:48:44 +0200

    смена ссылок

--- include/footer-politics.php
+++ include/footer-politics.php
@@ -1,7 +1,7 @@
 <div class="politics">
     <div>
-        <a href="#">Конфиденциальность</a>
-        <a href="/agreement/">Согласие на обработку персональных данных</a>
+        <a href="/terms_of_use/">Конфиденциальность</a>
+        <a href="/terms_of_use/">Согласие на обработку персональных данных</a>
     </div>
     <span>Webdoc © 2020 год</span>
 </div>
