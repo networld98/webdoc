922c1e95361550eeac5eb45d6a63a88be8fa7629
HP6300PRO <strecenko.sergej@gmail.com>
2021-03-05 10:15:22 +0200

    Вынес новый фильтр в отдельный шаблон

---
+++ local/components/webnauts/catalog.smart.filter/templates/search_filter_area_metro/lang/ru/template.php
@@ -0,0 +1,14 @@
+<?
+$MESS ['CT_BCSF_FILTER_TITLE'] = "Подбор параметров";
+$MESS ['CT_BCSF_FILTER_FROM'] = "От";
+$MESS ['CT_BCSF_FILTER_TO'] = "До";
+$MESS ['CT_BCSF_SET_FILTER'] = "Найти";
+$MESS ['CT_BCSF_DEL_FILTER'] = "Сбросить";
+$MESS ['CT_BCSF_FILTER_COUNT'] = "Выбрано: #ELEMENT_COUNT#";
+$MESS ['CT_BCSF_FILTER_SHOW'] = "Найти";
+$MESS ['CT_BCSF_FILTER_ALL'] = "Все";
+$MESS ['CT_BCSF_FILTER_CITY'] = "Город";
+$MESS ['CT_BCSF_FILTER_AREA'] = "Район";
+$MESS ['CT_BCSF_FILTER_METRO'] = "Метро";
+$MESS ['CT_BCSF_FILTER_SPECIALIZATION'] = "Специализация";
+?>
