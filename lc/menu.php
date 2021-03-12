830c798364dbf869439c1932b65a8528ca203409
HP6300PRO <strecenko.sergej@gmail.com>
2021-03-12 12:29:23 +0200

    Исправил вывод уведомлений из определенной киники

--- lc/menu.php
+++ lc/menu.php
@@ -27,18 +27,4 @@ $rsUser = CUser::GetByID($USER->GetID());
 	),
 	false
 ); ?>
-  <?/*  <div class="personal-cabinet-menu__manager mobile-display-none">
-        <div class="personal-cabinet-menu__manager__photo">
-            <img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/manager-photo.png" alt="photo">
-        </div>
-        <p class="personal-cabinet-menu__manager__desc">Мой персональный менеджер</p>
-        <p class="personal-cabinet-menu__manager__name">Марина Тихоновская</p>
-        <a href="tel:+79892897445" class="personal-cabinet-menu__manager__phone">+7 (989) 289-74-45</a>
-        <div class="personal-cabinet-menu__manager__social">
-            <a href=""><img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/viber-icon.svg" alt="icon"></a>
-            <a href=""><img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/whatsapp-icon.svg" alt="icon"></a>
-            <a href=""><img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/telegram-icon.svg" alt="icon"></a>
-        </div>
-        <a href="mailto:marina@prodoctorov.ru" class="personal-cabinet-menu__manager__email">marina@prodoctorov.ru</a>
-    </div>*/?>
 </div>
