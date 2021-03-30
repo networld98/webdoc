3f5c3584773c70bc548213208581149671280b82
s.stetsenko <s.stetsenko@ceteralabs.com>
2021-03-30 13:44:02 +0300

    Добавил скрипт обработки специализаций,районов во врачах

--- local/scripts/specializationDoctor.php
+++ local/scripts/specializationDoctor.php
@@ -2,34 +2,55 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 $iblock = 10;
 if(CModule::IncludeModule('iblock')) {
-    $arFilter = Array("IBLOCK_ID" => $iblock, "INCLUDE_SUBSECTIONS" => "Y", "PROPERTY_SPECIALIZATION_TECHNICAL_FIELD_VALUE" => false);
+    $arFilter = Array("IBLOCK_ID" => $iblock, "PROPERTY_SPECIALIZATION_TECHNICAL_FIELD_VALUE" => false);
     $res = CIBlockElement::GetList(Array(), $arFilter, false, array(), Array());
     while ($ob = $res->GetNextElement()) {
         $specClinicNames = [];
+        $doctorAddress = [];
+        $doctorArea= [];
+        $specClinic = [];
+        $doctorFullAddress = [];
         $Element = $ob->GetFields();
         $Prop = $ob->GetProperties();
-        if ($Prop['SPECIALIZATION_MAIN']['VALUE']!= NULL) {
-            $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_MAIN']['VALUE']);
-            if ($arEl = $obElement->GetNext())
-                $specClinicNames[] = $arEl["NAME"];
-        }
-        if ($Prop['SPECIALIZATION_DOP']['VALUE']!= NULL) {
-            $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_DOP']['VALUE']);
-            if ($arEl = $obElement->GetNext())
-                $specClinicNames[] = $arEl["NAME"];
-        }
-        if ($Prop['SPECIALIZATIONS']['VALUE'][0] != NULL) {
-            foreach ($Prop['SPECIALIZATIONS']['VALUE'] as $spec) {
-                $obElement = CIBlockElement::GetByID($spec);
-                if ($arEl = $obElement->GetNext())
-                    $specClinicNames[] = $arEl["NAME"];
+        if ($Prop['RECEPTION_ADDRESSES']['VALUE']!= NULL && $Prop['AREA']['VALUE']!= NULL) {
+            foreach ($Prop['RECEPTION_ADDRESSES']['VALUE'] as $key => $adr){
+                $doctorAddress[] = explode('/', $adr);
             }
-        }
-            $specClinicNamesText = implode(" ", $specClinicNames);
-            CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNamesText));
-            $f = fopen('specializationDoctor.log', 'a');
-            fwrite($f, date('d.m.Y H:i:s') . " Клинике " . $Element['NAME'] . " Прописалить специализации\n");
-            fclose($f);
+            foreach ($Prop['AREA']['VALUE'] as $key => $area){
+                $obElement = CIBlockSection::GetByID($area);
+                if ($arEl = $obElement->GetNext())
+                    $doctorArea[] = $arEl['NAME'];
+
+            }
+       }
+        foreach ($doctorAddress as $key => $item) {
+            if(count($item)!=3){
+                $doctorFullAddress[] = implode('/',array( $item[0],$item[1], $doctorArea[$key]));
+            }
+
+       }
+       if ($Prop['SPECIALIZATION_MAIN']['VALUE']!= NULL) {
+           $specClinic[] = $Prop['SPECIALIZATION_MAIN']['VALUE'];
+           $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_MAIN']['VALUE']);
+           if ($arEl = $obElement->GetNext())
+               $specClinicNames[] = $arEl["NAME"];
+       }
+       if ($Prop['SPECIALIZATION_DOP']['VALUE']!= NULL) {
+          $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
+           $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_DOP']['VALUE']);
+           if ($arEl = $obElement->GetNext())
+               $specClinicNames[] = $arEl["NAME"];
+       }
+       if ($Prop['SPECIALIZATIONS']['VALUE'][0] != NULL) {
+           $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
+           foreach ($Prop['SPECIALIZATIONS']['VALUE'] as $spec) {
+               $obElement = CIBlockElement::GetByID($spec);
+               if ($arEl = $obElement->GetNext())
+                   $specClinicNames[] = $arEl["NAME"];
+           }
+       }
+           $specClinicNamesText = implode(" ", $specClinicNames);
+           CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNamesText, "SPECIALIZATION_FULL" => $specClinic, "RECEPTION_ADDRESSES" => $doctorFullAddress));
     }
 }
 print "Отработал";
