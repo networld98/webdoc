0d8da8e77b07605a504b152b808cf84dc5085c88
HP6300PRO <strecenko.sergej@gmail.com>
2021-07-06 12:01:48 +0300

    Правки скриптов

--- local/scripts/specialization.php
+++ local/scripts/specialization.php
@@ -1,9 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 $iblock = 9;
+use \Bitrix\Main\Loader;
 if(CModule::IncludeModule('iblock')) {
+    $el = new CIBlockElement;
     $arFilter = Array("IBLOCK_ID" => $iblock, "INCLUDE_SUBSECTIONS" => "Y", "PROPERTY_SPECIALIZATION_TECHNICAL_FIELD_VALUE" => false);
-    $res = CIBlockElement::GetList(Array("RAND"=>"ASC"), $arFilter, false, array("nTopCount" => 2000), Array());
+    $res = CIBlockElement::GetList(Array("RAND"=>"ASC"), $arFilter, false, array("nTopCount" => 1500), Array());
     while ($ob = $res->GetNextElement()) {
         $specClinicNames = [];
         $Element = $ob->GetFields();
@@ -15,11 +17,14 @@ if(CModule::IncludeModule('iblock')) {
                     $specClinicNames[] = $arEl["NAME"];
             }
         }
-            CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNames));
+        $specClinicNamesText = implode(", ", $specClinicNames);
+            CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNamesText));
+            $el->Update($Element['ID'], Array('TIMESTAMP_X' => true));
+            \Bitrix\Iblock\PropertyIndex\Manager::updateElementIndex($iblock, $Element['ID']);
             $f = fopen('specialization.log', 'a');
             fwrite($f, date('d.m.Y H:i:s') . " Клинике " . $Element['NAME'] . " Прописалить специализации\n");
             fclose($f);
-        }
+    }
 }
 print "Отработал";
 ?>
--- local/scripts/specializationDoctor.php
+++ local/scripts/specializationDoctor.php
@@ -1,9 +1,11 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 $iblock = 10;
+use \Bitrix\Main\Loader;
 if(CModule::IncludeModule('iblock')) {
+    $el = new CIBlockElement;
     $arFilter = Array("IBLOCK_ID" => $iblock, "PROPERTY_SPECIALIZATION_TECHNICAL_FIELD_VALUE" => false);
-    $res = CIBlockElement::GetList(Array("RAND"=>"ASC"), $arFilter, false, array("nTopCount" => 2000), Array());
+    $res = CIBlockElement::GetList(Array("RAND"=>"ASC"), $arFilter, false, array("nTopCount" => 1500), Array());
     while ($ob = $res->GetNextElement()) {
         $specClinicNames = [];
         $doctorAddress = [];
@@ -23,7 +25,7 @@ if(CModule::IncludeModule('iblock')) {
                     $doctorArea[] = $arEl['NAME'];
 
             }
-       }
+        }
         if ($Prop['CITY']['VALUE']!= NULL) {
             foreach ($Prop['CITY']['VALUE'] as $key => $city){
                 $obElement = CIBlockSection::GetByID($city);
@@ -38,28 +40,31 @@ if(CModule::IncludeModule('iblock')) {
             }else{
                 $doctorFullAddress[] = implode('/',array( $item[0],$item[1], $doctorArea[$key]));
             }
-       }
-       if ($Prop['SPECIALIZATION_MAIN']['VALUE']!= NULL) {
-           $specClinic[] = $Prop['SPECIALIZATION_MAIN']['VALUE'];
-           $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_MAIN']['VALUE']);
-           if ($arEl = $obElement->GetNext())
-               $specClinicNames[] = $arEl["NAME"];
-       }
-       if ($Prop['SPECIALIZATION_DOP']['VALUE']!= NULL) {
-          $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
-           $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_DOP']['VALUE']);
-           if ($arEl = $obElement->GetNext())
-               $specClinicNames[] = $arEl["NAME"];
-       }
-       if ($Prop['SPECIALIZATIONS']['VALUE'][0] != NULL) {
-           $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
-           foreach ($Prop['SPECIALIZATIONS']['VALUE'] as $spec) {
-               $obElement = CIBlockElement::GetByID($spec);
-               if ($arEl = $obElement->GetNext())
-                   $specClinicNames[] = $arEl["NAME"];
-           }
-       }
-           CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNames, "SPECIALIZATION_FULL" => $specClinic, "RECEPTION_ADDRESSES" => $doctorFullAddress));
+        }
+        if ($Prop['SPECIALIZATION_MAIN']['VALUE']!= NULL) {
+            $specClinic[] = $Prop['SPECIALIZATION_MAIN']['VALUE'];
+            $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_MAIN']['VALUE']);
+            if ($arEl = $obElement->GetNext())
+                $specClinicNames[] = $arEl["NAME"];
+        }
+        if ($Prop['SPECIALIZATION_DOP']['VALUE']!= NULL) {
+            $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
+            $obElement = CIBlockSection::GetByID($Prop['SPECIALIZATION_DOP']['VALUE']);
+            if ($arEl = $obElement->GetNext())
+                $specClinicNames[] = $arEl["NAME"];
+        }
+        if ($Prop['SPECIALIZATIONS']['VALUE'][0] != NULL) {
+            $specClinic[] = $Prop['SPECIALIZATION_DOP']['VALUE'];
+            foreach ($Prop['SPECIALIZATIONS']['VALUE'] as $spec) {
+                $obElement = CIBlockElement::GetByID($spec);
+                if ($arEl = $obElement->GetNext())
+                    $specClinicNames[] = $arEl["NAME"];
+            }
+        }
+        $specClinicNamesText = implode(", ", $specClinicNames);
+        CIBlockElement::SetPropertyValuesEx($Element['ID'], false, array("SPECIALIZATION_TECHNICAL_FIELD" => $specClinicNamesText, "SPECIALIZATION_FULL" => $specClinic, "RECEPTION_ADDRESSES" => $doctorFullAddress));
+        $el->Update($Element['ID'], Array('TIMESTAMP_X' => true));
+        \Bitrix\Iblock\PropertyIndex\Manager::updateElementIndex($iblock, $Element['ID']);
     }
 }
 print "Отработал";
