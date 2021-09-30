543865ffe5277e76300c8d9716084eaed6e84e9e
Serhii Honcharuk <serhiihoncharuk@gmail.com>
2021-09-30 11:09:37 +0300

    Поправил чекбоксы фильтра на главной

--- local/components/webnauts/catalog.smart.filter/templates/search_filter_area_metro/template.php
+++ local/components/webnauts/catalog.smart.filter/templates/search_filter_area_metro/template.php
@@ -648,7 +648,7 @@ while($ob = $res->GetNextElement()){
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-sm-12">
+                <div class="col-12 col-sm-12 custom-col-box">
                     <?foreach($arResult["HIDDEN"] as $arItem):?>
                         <input type="hidden" name="<?echo $arItem["CONTROL_NAME"]?>" id="<?echo $arItem["CONTROL_ID"]?>" value="<?echo $arItem["HTML_VALUE"]?>" />
                     <?endforeach;
